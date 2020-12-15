b0bf6ba5ed6cc765f33a559bd8aa07547b05d043
logismiko99 <synology@321>
2020-12-15 11:56:49 +0530

    update gitignore

--- DWUIntranet/DWUIntranet/login.aspx.designer.cs
+++ DWUIntranet/DWUIntranet/login.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace DWUIntranet {
     public partial class login {
         
         /// <summary>
-        /// form2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
-        
-        /// <summary>
         /// txtUsername control.
         /// </summary>
         /// <remarks>
