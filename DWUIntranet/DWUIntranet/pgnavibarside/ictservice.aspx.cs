92b95f4351a028dd2c096c4f6c45fe2557af44f1
Madushan <unm.madushan@gmail.com>
2021-01-20 19:35:23 +0800

    change Ict

--- DWUIntranet/DWUIntranet/pgnavibarside/ictservice.aspx.cs
+++ DWUIntranet/DWUIntranet/pgnavibarside/ictservice.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace DWUIntranet.pgnavibarside
@@ -11,7 +12,9 @@ namespace DWUIntranet.pgnavibarside
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            (Page.Master.FindControl("activeict") as HtmlControl).Attributes.Add("class", "active");
+            (Page.Master.FindControl("Label1") as Label).Text = "ICT Services";
+            (Page.Master.FindControl("sidedash") as HtmlAnchor).HRef = "~/pghome/home.aspx";
         }
     }
 }
