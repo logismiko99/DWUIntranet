fc3caa1926bc1ff54c82d932d147dc7a870a7602
Madushan <unm.madushan@gmail.com>
2021-02-19 17:42:02 +0800

    event content class

---
+++ DWUIntranet/DAL/Models/EventContent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class EventContent
+    {
+        public string EventTitle { get; set; }
+        public string Venue { get; set; }
+
+        public string  Category { get; set; }
+
+        public DateTime FromDate { get; set; }
+
+        public DateTime ToDate { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+
+
+        public string AllowRegistration { get; set; }
+        public string AllowUnRegistration { get; set; }
+        public string Repetition { get; set; }
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
+    }
+}
